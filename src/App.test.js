3f5f9cdc9ba0d5491c2c1212a834069c1904f7cc
Patric <patric@patriccampbell.com>
2017-11-21 11:44:12 -0500

    Add props to Table component test

--- src/App.test.js
+++ src/App.test.js
@@ -59,9 +59,11 @@ describe("Table", () => {
       { title: "1", author: "1", num_comments: 1, points: 2, objectID: "y" },
       { title: "2", author: "2", num_comments: 1, points: 2, objectID: "z" },
     ],
+    sortKey: "TITLE",
+    isSortReverse: false,
   };
 
-  it("renders without crashig", () => {
+  it("renders without crashing", () => {
     const div = document.createElement("div");
     ReactDOM.render(<Table {...props} />, div);
   });
